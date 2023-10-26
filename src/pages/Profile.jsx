aa32c5618f9571fa1fc6d62a67d6cda416b2757a
faisalsaid <falsdesain@gmail.com>
2023-10-26 15:58:02 +0900

    add simple profile ui

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,5 +2,13 @@ import { useSelector } from 'react-redux';
 
 export default function Profile() {
   const { data, isLoading, error } = useSelector((state) => state.user.curentUser);
-  return <div>Profile</div>;
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-semibold text-slate-700 py-4">Profile</h1>
+      <div>
+        <p>{data.name}</p>
+        <img className="rounded-full" src={data.avatar} alt="profile picture" />
+      </div>
+    </div>
+  );
 }
