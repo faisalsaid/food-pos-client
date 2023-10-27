6df3f83ed8620eac11bec2e2d2265dfea36f2654
faisalsaid <falsdesain@gmail.com>
2023-10-28 07:27:59 +0900

    update api uri

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-const uri = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cyclic-api-demo.cyclic.cloud/';
+const uri = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://cyclic-api-demo.cyclic.cloud';
 
 // https://vitejs.dev/config/
 export default defineConfig({
