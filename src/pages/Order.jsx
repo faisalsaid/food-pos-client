6a7f313b3af94de05a910b4f9be5bd6b8017b0a5
faisalsaid <falsdesain@gmail.com>
2023-11-04 12:01:26 +0900

    create order componen, fix menu card grid

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -6,8 +6,9 @@ import { IoNotifications, IoIceCreamOutline } from 'react-icons/io5';
 import { CgMenuGridO } from 'react-icons/cg';
 import { PiHamburger, PiCoffee } from 'react-icons/pi';
 import { BiBowlRice } from 'react-icons/bi';
+import { RiDeleteBinLine } from 'react-icons/ri';
 import { MdOutlineLocalDrink } from 'react-icons/md';
-import { BiPlus, BiMinus, BiAddToQueue } from 'react-icons/bi';
+import { BiPlus, BiMinus, BiAddToQueue, BiReset, BiEdit, BiUser, BiEditAlt } from 'react-icons/bi';
 
 const listTag = [
   {
@@ -66,10 +67,10 @@ const TagMenu = ({ tag, tagActive }) => {
 
 const MenuCard = () => {
   return (
-    <div className="max-w-[300px] bg-white p-3 rounded-lg">
+    <div className=" bg-white p-3 rounded-lg">
       <div className="flex gap-2">
         <img
-          className="w-24 h-24 object-cover rounded-md"
+          className="w-24 h-24 object-cover rounded-xl"
           src="https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/62c59f6365a259b03da440de3973f201/Derivates/668cba6648888c61d249c0a5d9651157a4ce3793.jpg"
           alt=""
         />
@@ -99,11 +100,41 @@ const MenuCard = () => {
   );
 };
 
+const ListOrderCard = ({ order }) => {
+  return (
+    <div className="flex gap-2 min-w-[250px] ">
+      <img
+        className="w-12 h-12 object-cover rounded-xl"
+        src="https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/62c59f6365a259b03da440de3973f201/Derivates/668cba6648888c61d249c0a5d9651157a4ce3793.jpg"
+        alt="order_image"
+      />
+      <div className="flex flex-col justify-between w-full">
+        <p className="text-sm">Pizza Tandori Paneer</p>
+        <div className="flex gap-2 items-center text-sm">
+          <button className="text-red-700">
+            <RiDeleteBinLine />
+          </button>
+          <p className="flex-1 font-semibold text-sm">$150,00</p>
+          <div className="flex items-center gap-2">
+            <button className="  bg-orange-400 hover:bg-orange-500 flex justify-center content-center text-white p-[2px] rounded-lg">
+              <BiMinus />
+            </button>
+            <span>1</span>
+            <button className="  bg-orange-400 hover:bg-orange-500 flex justify-center content-center text-white p-[2px] rounded-lg">
+              <BiPlus />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const tagActive = 'all';
 
 export default function Order() {
   return (
-    <div className=" flex gap-6 ">
+    <div className=" flex ">
       <div className="flex-1 p-6">
         <div className=" flex justify-between">
           <div className="">
@@ -128,7 +159,7 @@ export default function Order() {
             <TagMenu key={i} tag={list} tagActive={tagActive} />
           ))}
         </div>
-        <div className="flex gap-3 flex-wrap">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 ">
           <MenuCard />
           <MenuCard />
           <MenuCard />
@@ -137,7 +168,70 @@ export default function Order() {
           <MenuCard />
         </div>
       </div>
-      <div className="max-w-fit bg-white p-4  ">List Order</div>
+      <div className="max-w-fit bg-white p-4 flex flex-col gap-4">
+        <div className="flex items-center gap-2 text-sm">
+          <button className="flex-1 flex items-center gap-2 justify-center bg-red-500 hover:bg-red-600 text-white py-1 rounded-lg px-2">
+            <BiReset />
+            <span>Reset</span>
+          </button>
+          <button className="flex-1 flex items-center gap-2 justify-center bg-green-500 hover:bg-green-600 text-white py-1 rounded-lg min-w-fit px-2">
+            <BiEdit />
+            <span>Create Order</span>
+          </button>
+        </div>
+        <div className="flex justify-between">
+          <p>Order #645</p>
+          <p className="text-sm text-slate-400">Date</p>
+        </div>
+        <div>
+          <p className="font-semibold mb-1">Customer Information</p>
+          <div>
+            <div className="flex items-center text-sm gap-2 text-slate-400 border py-1 px-2 rounded-md mb-1">
+              <BiUser />
+              <input className="outline-none" type="text" placeholder="Customer Name" />
+            </div>
+            <div className="flex items-center text-sm gap-2 text-slate-400 border py-1 px-2 rounded-md">
+              <BiEditAlt />
+              <input className="outline-nonet" type="text" placeholder="Table" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="font-semibold">Order Details</p>
+          <div className="flex flex-col gap-2">
+            {[1, 2, 3].map(() => (
+              <ListOrderCard />
+            ))}
+          </div>
+        </div>
+        <div className="">
+          <div className="flex bg-green-50 py-1 px-2 rounded-lg text-xs gap-2 mb-2">
+            <span className="flex-1 font-semibold">Add</span>
+            <button className="text-slate-500">Discound</button>
+            <button className="text-slate-500">Coupon Code</button>
+            <button className="text-slate-500">Note</button>
+          </div>
+          <div className="bg-slate-50 p-2 rounded-lg text-xs border-b">
+            <div className="flex items-center justify-between">
+              <p>Subtotal</p>
+              <p>$400.00</p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p>Discount sales</p>
+              <p>-$20.00</p>
+            </div>
+            <div className="flex items-center justify-between">
+              <p>Total sale tax</p>
+              <p>$20.00</p>
+            </div>
+          </div>
+          <div className="bg-slate-50 p-2 rounded-md flex justify-between">
+            <p>Total </p>
+            <p>$180.00</p>
+          </div>
+          <button className="bg-green-500 text-white justify-center w-full items-center rounded-lg py-1 px-2 hover:bg-green-600">Payment Proccess</button>
+        </div>
+      </div>
     </div>
   );
 }
