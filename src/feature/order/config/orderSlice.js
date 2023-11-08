6092c143442513ee0662e7bcf2df2d7ed4797e65
faisalsaid <falsdesain@gmail.com>
2023-11-08 17:48:56 +0900

    generte oderSlite

---
+++ src/feature/order/config/orderSlice.js
@@ -0,0 +1,17 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+// init state
+const initialState = {
+  order: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+// create user slice
+const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {},
+});
