5b819076679a743b99d889c2d00731d60c8d0051
faisalsaid <falsdesain@gmail.com>
2023-11-11 01:05:25 +0900

    populate purhcase detail modal

--- src/feature/purchase/comps/PurchaseModal.jsx
+++ src/feature/purchase/comps/PurchaseModal.jsx
@@ -22,19 +22,16 @@ const customStyles = {
   },
 };
 
-const ListOrder = () => {
+const ListOrder = ({ info }) => {
+  console.log(info);
   return (
     <div className="flex gap-2">
-      <img
-        className="w-14 h-14 object-cover rounded-xl"
-        src="https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/62c59f6365a259b03da440de3973f201/Derivates/668cba6648888c61d249c0a5d9651157a4ce3793.jpg"
-        alt=""
-      />
+      <img className="w-14 h-14 object-cover rounded-xl" src={info?.item?.image?.url} alt="menu image" />
       <div className="flex-1 flex  flex-col">
-        <p className="text-sm text-slate-600 font-semibold">Pizza Tandorri Paineer</p>
-        <p className="text-xs flex-1 text-slate-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit...</p>
+        <p className="text-sm text-slate-600 font-semibold">{info?.item?.title}</p>
+        <p className="text-xs flex-1 text-slate-400">{info?.item?.description}</p>
         <div className="flex justify-between items-center text-xs">
-          <p>1 items</p> <p className="font-bold">$120.00</p>
+          <p>{info?.quantity}</p> <p className="font-bold">${info?.orderPrice.toFixed(2)}</p>
         </div>
       </div>
     </div>
@@ -42,11 +39,13 @@ const ListOrder = () => {
 };
 
 export default function PurchaseModal({ isOpen, closeModel, content }) {
+  const { listOrder } = content;
+  console.log(content);
   return (
     <Modal isOpen={isOpen} onRequestClose={closeModel} contentLabel="Purchase Modal" style={customStyles}>
       <div className="w-[500px] ">
         <div className="flex items-center gap-2  border-b pb-2 ">
-          <p className="flex-1 font-semibold">Paymen Order #{content?.order}</p>
+          <p className="flex-1 font-semibold">Paymen Order #{content?.orderRef}</p>
           <p className="text-slate-400 text-sm">Sunday, 01 Januari 2001</p>
           <button onClick={closeModel} className="bg-red-600 p-1 text-white rounded-md">
             <AiOutlineClose />
@@ -55,8 +54,8 @@ export default function PurchaseModal({ isOpen, closeModel, content }) {
         <div className="py-2 border-b">
           <p className="">Order Details</p>
           <div className="py-2 flex flex-col gap-2">
-            {[1, 2, 3, 4].map((data, i) => (
-              <ListOrder key={i} />
+            {listOrder?.map((data, i) => (
+              <ListOrder key={i} info={data} />
             ))}
           </div>
         </div>
@@ -67,7 +66,7 @@ export default function PurchaseModal({ isOpen, closeModel, content }) {
               <span>
                 <BiUser />
               </span>
-              <span>{content?.customer_name}</span>
+              <span>{content?.customerName}</span>
             </p>
             <p className="flex gap-2 items-center">
               <span>
@@ -77,32 +76,33 @@ export default function PurchaseModal({ isOpen, closeModel, content }) {
             </p>
             <p className="flex gap-2 items-center">
               <span>
-                {' '}
                 <BiWallet />
               </span>
-              <span>Cash</span>
+              <span>{content?.paymentMethod}</span>
             </p>
           </div>
         </div>
         <div>
           <div className="bg-slate-100 p-2 rounded-lg text-slate-400 border-b">
             <div className="flex items-center justify-between">
-              <p>Subtotal</p> <p>$200.00</p>
+              <p>Subtotal</p> <p>${content?.subtotal?.toFixed(2)}</p>
             </div>
             <div className="flex items-center justify-between">
-              <p>Discount sales</p> <p>-$20.00</p>
+              <p>Discount sales</p> <p>-${content?.discountSales?.toFixed(2)}</p>
             </div>
             <div className="flex items-center justify-between">
-              <p>Total sales tax</p> <p>$2.00</p>
+              <p>Total sales tax</p> <p>${content?.saleTax?.toFixed(2)}</p>
             </div>
           </div>
           <div className="bg-slate-100 p-2 rounded-lg">
             <div className="flex items-center justify-between font-semibold text-xl">
-              <p>Total</p> <p>${content.price}</p>
+              <p>Total</p> <p>${content?.finalPrice?.toFixed(2)}</p>
             </div>
-            <div className="flex items-center justify-between">
-              <p>Change</p> <p>$40.00</p>
-            </div>
+            {content?.change && (
+              <div className="flex items-center justify-between">
+                <p>Change</p> <p>${content?.change?.toFixed(2)}</p>
+              </div>
+            )}
           </div>
         </div>
         <div className="py-2 flex justify-end">
