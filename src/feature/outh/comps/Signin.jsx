a5e9a865894b9f3b19aff2f62f658e8b4ed0b2fe
faisalsaid <falsdesain@gmail.com>
2023-11-13 11:37:56 +0900

    fix signin and up loyout

--- src/feature/outh/comps/Signin.jsx
+++ src/feature/outh/comps/Signin.jsx
@@ -53,44 +53,46 @@ export const Signin = () => {
   };
 
   return (
-    <div className="container mx-auto w-96">
-      <h1 className="text-3xl text-center font-semibold py-7 text-slate-700">Sign In</h1>
-      <div className="mx-auto w-96">
-        <form onSubmit={formik.handleSubmit}>
-          <input
-            type="email"
-            placeholder="email"
-            className={`border p-3 rounded-md w-full mb-2  ${formik?.errors?.email && 'border-red-500'} `}
-            name="email"
-            id="email"
-            aria-label="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          ></input>
-          {formik?.errors?.email && <div className="mb-2 text-xs rounded-sm bg-red-100 p-1 text-red-900">{formik?.errors?.email}</div>}
-          <input
-            type="password"
-            placeholder="password"
-            className={`border p-3 rounded-md w-full mb-2  ${formik?.errors?.password && 'border-red-500'} `}
-            name="password"
-            id="password"
-            aria-label="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-          ></input>
-          {formik?.errors?.password && <div className="mb-2 text-xs rounded-sm bg-red-100 p-1 text-red-900">{formik?.errors?.password}</div>}
-          <button type="submit" className="text-center bg-teal-600 text-white p-2 w-full rounded-md hover:bg-teal-700 uppercase">
-            {isLoading ? 'Loading ...' : 'Sign In'}
-          </button>
-        </form>
-        {isError && <div className="bg-red-100 text-red-900 py-1 px-2 mt-2 text-sm">{isError}</div>}
-        <OAuth />
-      </div>
-      <div className="text-sm mt-2 flex gap-2">
-        <span className="text-slate-600">Dont have an acount?</span>
-        <Link to={'/signup'}>
-          <span className="text-blue-600">Sign up</span>
-        </Link>
+    <div className="container mx-auto px-3  h-screen flex flex-col justify-center items-center content-center">
+      <div className=" sm:w-96">
+        <h1 className="text-3xl text-center font-semibold py-7 text-slate-700">Sign In</h1>
+        <div className="mx-auto ">
+          <form onSubmit={formik.handleSubmit}>
+            <input
+              type="email"
+              placeholder="email"
+              className={`border p-3 rounded-md w-full mb-2  ${formik?.errors?.email && 'border-red-500'} `}
+              name="email"
+              id="email"
+              aria-label="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            ></input>
+            {formik?.errors?.email && <div className="mb-2 text-xs rounded-sm bg-red-100 p-1 text-red-900">{formik?.errors?.email}</div>}
+            <input
+              type="password"
+              placeholder="password"
+              className={`border p-3 rounded-md w-full mb-2  ${formik?.errors?.password && 'border-red-500'} `}
+              name="password"
+              id="password"
+              aria-label="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            ></input>
+            {formik?.errors?.password && <div className="mb-2 text-xs rounded-sm bg-red-100 p-1 text-red-900">{formik?.errors?.password}</div>}
+            <button type="submit" className="text-center bg-teal-600 text-white p-2 w-full rounded-md hover:bg-teal-700 uppercase">
+              {isLoading ? 'Loading ...' : 'Sign In'}
+            </button>
+          </form>
+          {isError && <div className="bg-red-100 text-red-900 py-1 px-2 mt-2 text-sm">{isError}</div>}
+          <OAuth />
+        </div>
+        <div className="text-sm mt-2 flex gap-2">
+          <span className="text-slate-600">Dont have an acount?</span>
+          <Link to={'/signup'}>
+            <span className="text-blue-600">Sign up</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
