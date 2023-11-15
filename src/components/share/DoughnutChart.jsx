3bea48fb2c428d583c006d420f4f59aec5a50f82
faisalsaid <falsdesain@gmail.com>
2023-11-15 14:54:05 +0900

    add ? mark to data not ready

--- src/components/share/DoughnutChart.jsx
+++ src/components/share/DoughnutChart.jsx
@@ -11,11 +11,11 @@ export default function DoughnutChart({ topMenu }) {
   console.log(topMenu);
 
   const data = {
-    labels: topMenu.map((menu) => menu.menuTitle),
+    labels: topMenu?.map((menu) => menu.menuTitle),
     datasets: [
       {
         label: '# Best of 5',
-        data: topMenu.map((menu) => menu.totalQuantity),
+        data: topMenu?.map((menu) => menu.totalQuantity),
         backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)'],
       },
     ],
