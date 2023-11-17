95baeeca39630563a70c9225db8a64ff09ad2aca
faisalsaid <falsdesain@gmail.com>
2023-11-17 10:38:15 +0900

    set 2col menu if md breakpoint

--- src/feature/order/comps/MenusComp.jsx
+++ src/feature/order/comps/MenusComp.jsx
@@ -108,7 +108,7 @@ export default function MenusComp() {
           <TagMenu key={i} tag={list} tagActive={tagActive} setTag={filterDisplay} />
         ))}
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 ">
+      <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 gap-3 ">
         {menuDisplay.map((menu, i) => (
           <MenuCard key={i} menuInfo={menu} />
         ))}
