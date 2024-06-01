50cc08939e22d4b1f5decfb4f1a7824d199f9479
faisalsaid <falsdesain@gmail.com>
2024-06-01 21:47:15 +0900

    api environtment

--- src/config/environtment.js
+++ src/config/environtment.js
@@ -1,2 +1,2 @@
-export const apiURI = import.meta.env.VITE_STATUS_MODE === 'prod' ? 'https://cyclic-api-demo.cyclic.cloud/api' : 'http://localhost:3000/api';
+export const apiURI = import.meta.env.VITE_STATUS_MODE === 'prod' ? 'https://pos-api-ohvj.onrender.com/api' : 'http://localhost:3000/api';
 console.log(apiURI);
